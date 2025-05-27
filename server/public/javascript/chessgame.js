ad1f13841e00a1f149cbf2f2691bcb8a994fcd1f
Kotla Lokeshwari <kotlalokeshwari1871@gmail.com>
2025-05-27 17:33:33 +0530

    small changes

--- server/public/javascript/chessgame.js
+++ server/public/javascript/chessgame.js
@@ -1,6 +1,7 @@
 // alert("helloo")
 const socket=io()
 // connect to backend
+const chess=new Chess()
 
 socket.emit("w")
 // sending to backend the biryani
@@ -9,6 +10,9 @@ socket.on("w",()=>{
     //  all receving from backend
     console.log("You are white")
 })
+
+socket.emit("b")
+
 socket.on("b",()=>{
     //  all receving from backend
     console.log("You are black")
